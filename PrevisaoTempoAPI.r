67aa8c79a82104139a30db0a5081d59ca8b70f94
junior.silva <junior.silva@ibratan.com.br>
2024-09-17 21:19:53 -0300

    Finalizando o projeto.

--- PrevisaoTempoAPI.r
+++ PrevisaoTempoAPI.r
@@ -1,24 +1,17 @@
 #install.packages("httr")
-
 library(httr)
 library(jsonlite)
 
-
-api_key <- "a5d5e206a89e10d64cc65740be76cf95"
-
+api_key <- "SUA_CHAVE_API"
 cidade <- "Santos"
 pais <- "BR"
 
 url <- paste0("http://api.openweathermap.org/data/2.5/weather?q=", cidade, ",", pais, "&appid=", api_key, "&units=metric")
-print(url)
-
 response <- GET(url)
-print(response)
 
 if (response$status_code == 200) {
-   data <- fromJSON(content(response, "text"))
-  
-  # Exibir dados principais do tempo
+  data <- fromJSON(content(response, "text"))
+
   cat('***************\n')
   cat("Previsão do Tempo para a cidade de", cidade, "agora:\n")
   cat("Temperatura: ", data$main$temp, "°C\n")
